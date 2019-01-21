34361d25fc7afd649ff4d2ee838b8a4b4d6d8a54
JY <pyeonjy97@gmail.com>
2019-01-22 01:04:14 +0900

    add Toolbar

--- ShowBlocks/app/src/main/java/com/example/showblocks/MainActivity.java
+++ ShowBlocks/app/src/main/java/com/example/showblocks/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.showblocks;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.showblocks.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(binding.toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_reload) {
+            Log.d(TAG, "selected");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
