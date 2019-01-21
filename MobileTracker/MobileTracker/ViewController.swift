ecab0a2bd823a417bd0d693d19262bf07389b245
Seungyeon Lee <adie0423@gmail.com>
2019-01-21 23:06:54 +0900

    Add initial project

---
+++ MobileTracker/MobileTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileTracker
+//
+//  Created by Seungyeon Lee on 21/01/2019.
+//  Copyright © 2019 Seungyeon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
