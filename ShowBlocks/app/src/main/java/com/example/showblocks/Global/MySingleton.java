143d60695c524adefa636cd79be195c1fcfc9133
JY <pyeonjy97@gmail.com>
2019-01-23 23:11:56 +0900

    requestQueue에 finish callback listener 달기

--- ShowBlocks/app/src/main/java/com/example/showblocks/Global/MySingleton.java
+++ ShowBlocks/app/src/main/java/com/example/showblocks/Global/MySingleton.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.example.showblocks.Event.FetchedBlockEvent;
+
+import org.greenrobot.eventbus.EventBus;
 
 public class MySingleton {
     private static MySingleton mInstance;
     private static Context mCtx;
     private RequestQueue mRequestQueue;
+    private static final String TAG = MySingleton.class.getName();
 
     public MySingleton(Context context) {
         mCtx = context;
@@ -26,6 +30,12 @@ public class MySingleton {
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
             mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+            mRequestQueue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
+                @Override
+                public void onRequestFinished(Request<Object> request) {
+                    EventBus.getDefault().post(new FetchedBlockEvent(request.getTag().toString()));
+                }
+            });
         }
         return mRequestQueue;
     }
