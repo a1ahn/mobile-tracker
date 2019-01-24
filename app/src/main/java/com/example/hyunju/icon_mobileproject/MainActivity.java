1b70b6a8e8eb165e1351393873b552fa093d1a5a
Hyunju1 <hh960305@naver.com>
2019-01-24 23:49:41 +0900

    create transaction detail page

--- app/src/main/java/com/example/hyunju/icon_mobileproject/MainActivity.java
+++ app/src/main/java/com/example/hyunju/icon_mobileproject/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.hyunju.icon_mobileproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +35,16 @@ public class MainActivity extends AppCompatActivity {
 
 
             listview.setAdapter(itemsAdapter);
-
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position,
+                                        long id) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    String message = "abc";
+                    intent.putExtra(EXTRA_MESSAGE, message);
+                    startActivity(intent);
+                }
+            });
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
