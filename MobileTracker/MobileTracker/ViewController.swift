5428d7e2469fc5d670e5fa64b169a4a96f7feae9
byunsangjin <qustkdwls1@naver.com>
2019-03-28 17:48:13 +0900

    Project init

---
+++ MobileTracker/MobileTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileTracker
+//
+//  Created by Byunsangjin on 28/03/2019.
+//  Copyright © 2019 Byunsangjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
