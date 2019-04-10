606bf7c2a84e4088810dee8e8cdc4e4cf067fdbb
nachoi <skchl13@gmail.com>
2019-04-10 18:42:50 +0900

    Init

---
+++ app/src/main/java/io/yena/mobiletracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.yena.mobiletracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
