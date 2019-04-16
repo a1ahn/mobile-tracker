43f66528f8d8867548987281f0af6b1703069350
mellbrown <2mellbrown@gmail.com>
2019-04-16 18:42:09 +0900

    block list activity add navigation drawer

--- MobileTracker/app/src/main/java/me/myds/g2u/mobiletracker/BlockListActivity.java
+++ MobileTracker/app/src/main/java/me/myds/g2u/mobiletracker/BlockListActivity.java
@@ -1,16 +1,18 @@
 package me.myds.g2u.mobiletracker;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.ViewGroup;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
@@ -31,8 +33,13 @@ import me.myds.g2u.mobiletracker.utill.BlockViewHolder;
 
 public class BlockListActivity extends AppCompatActivity {
 
-    private static final String TAG = "BlockListActivity";
+    private static final String TAG = "orgBlockListActivity";
     private static final int COMPLETE_LOAD_BLOCKS = 22;
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
     private TextView txtIndicate;
     private SwipeRefreshLayout mSwipe;
@@ -44,6 +51,26 @@ public class BlockListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_block_list);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawer = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_saved_blocks:
+                    break;
+            }
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        });
 
         txtIndicate = findViewById(R.id.txtIndicate);
         txtIndicate.setText("0 block loaded");
@@ -139,4 +166,13 @@ public class BlockListActivity extends AppCompatActivity {
             }
         }
     };
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
